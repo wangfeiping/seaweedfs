24e4e05d9f64c2a1fb8e257601bccaf28f846887
wangfeiping <wangfeiping@outlook.com>
2017-03-01 18:20:04 +0800

    支持redis 3 cluster，支持增删事务

--- weed/filer/redis_store/redis_cluster_store.go
+++ weed/filer/redis_store/redis_cluster_store.go
@@ -2,7 +2,7 @@ package redis_store
 
 import (
 	"github.com/chrislusf/seaweedfs/weed/filer"
-//        "fmt"
+	"github.com/chrislusf/seaweedfs/weed/glog"
 
 	redis "gopkg.in/redis.v5"
 )
@@ -24,31 +24,64 @@ func (s *RedisClusterStore) Get(fullFileName string) (fid string, err error) {
 		err = filer.ErrNotFound
 	}
 	return fid, err
-
-//      pong, err := s.Client.Ping().Result()
-//	fmt.Println(pong, err)
-//      val, err := s.Client.Get("key").Result()
-//	if err != nil {
-//		panic(err)
-//	}
-//	fmt.Println("key", val)
-//        return "", nil
 }
+
 func (s *RedisClusterStore) Put(fullFileName string, fid string) (err error) {
-	_, err = s.Client.Set(fullFileName, fid, 0).Result()
-	if err == redis.Nil {
-		err = nil
+//	_, err = s.Client.Set(fullFileName, fid, 0).Result()
+//	if err == redis.Nil {
+//		err = nil
+//	}
+//	return err
+	pipe := s.Client.TxPipeline()
+	if err = pipe.Set(fullFileName, fid, 0).Err(); err != nil {
+		e := pipe.Discard()
+		if e != nil {
+			glog.Fatalf("redis txpipeline discard error! file = %s fid = %s : %v", fullFileName, fid, e)
+		}
+	}else if err = pipe.Set(fid, fullFileName, 0).Err(); err != nil {
+		e := pipe.Discard()
+		if e != nil {
+                	glog.Fatalf("redis txpipeline discard error! fid = %s file = %s : %v", fullFileName, fid, e)
+		}
 	}
-	return err
+	_, e := pipe.Exec()
+	if e != nil {
+		glog.Fatalf("redis txpipeline exec error! %s %s : %v", fullFileName, fid, e)
+	}
+	glog.Infof("redis tx put %s %s", fullFileName, fid)
+	return
 }
 
 // Currently the fid is not returned
 func (s *RedisClusterStore) Delete(fullFileName string) (err error) {
-	_, err = s.Client.Del(fullFileName).Result()
-	if err == redis.Nil {
-		err = nil
+//	_, err = s.Client.Del(fullFileName).Result()
+//	if err == redis.Nil {
+//		err = nil
+//	}
+//	return err
+	var fid string
+	fid, err = s.Get(fullFileName)
+	if err != nil {
+		return
 	}
-	return err
+	pipe := s.Client.TxPipeline()
+	if err = pipe.Del(fullFileName).Err(); err != nil {
+                e := pipe.Discard()
+                if e != nil {
+                        glog.Fatalf("redis txpipeline delete discard error! file = %s fid = %s : %v", fullFileName, fid, e)
+                }
+        }else if err = pipe.Del(fid).Err(); err != nil {
+                e := pipe.Discard()
+                if e != nil {
+                        glog.Fatalf("redis txpipeline delete discard error! fid = %s file = %s : %v", fullFileName, fid, e)
+                }
+        }
+        _, e := pipe.Exec()
+        if e != nil {
+                glog.Fatalf("redis txpipeline delete exec error! %s %s : %v", fullFileName, fid, e)
+        }
+	glog.Infof("redis tx delete %s %s", fullFileName, fid)
+        return
 }
 
 func (s *RedisClusterStore) Close() {
